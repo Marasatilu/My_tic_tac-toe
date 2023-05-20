497fab16741e636594ea2cc8c2df4fa0606dace4
marasatilu <marasatilu@gmail.com>
2023-05-20 18:57:23 +0200

    add an arry to useState

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -2,15 +2,17 @@ import { useState } from "react";
 import Cell from "./components/Cell";
 
 const App = () => {
-  return (
-    <div className="app">
-      <div className="gameboard">
-        <Cell />
-        
-        </div>
-        <p></p>
-    </div>
-  );
+
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""])
+    return (
+      <div className="app">
+        <div className="gameboard">
+          <Cell />
+          
+          </div>
+          <p></p>
+      </div>
+    );
 }
 
 export default App;
